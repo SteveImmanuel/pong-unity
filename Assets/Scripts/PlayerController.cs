6a78d930b66f9d57299a456eb1bb4b18554712d0
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-01-27 15:36:59 +0700

    Implement paddle powerup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,25 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20f;
-    public float yBoundary = 9f;
     public KeyCode upButton = KeyCode.UpArrow;
     public KeyCode downButton = KeyCode.DownArrow;
+    public KeyCode transformButton = KeyCode.E;
+    public float speed = 20f;
+    public float yBoundary = 9f;
+    public float maxLengthScale = 1.5f;
+    public float transformDuration = 0.5f;
+    public float powerUpDuration = 1;
+    public float powerUpCoolDown = 5;
 
     private Rigidbody2D rb;
     private float currentVelocity;
     private ContactPoint2D lastContactPoint;
+    private bool powerAvailable = true;
+    private bool isPowerActivated = false;
+    private float elapsedTime = 0f;
 
     void Start()
     {
@@ -19,6 +29,8 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        elapsedTime += Time.deltaTime;
+
         if (Input.GetKey(upButton))
         {
             currentVelocity = speed;
@@ -33,8 +45,42 @@ public class PlayerController : MonoBehaviour
         }
         Vector3 position = transform.position;
         position.y = Mathf.Clamp(position.y, -yBoundary, yBoundary);
-
         transform.position = position;
+
+
+        if (Input.GetKeyDown(transformButton) && powerAvailable && !isPowerActivated)
+        {
+            isPowerActivated = true;
+            StartCoroutine(Transform(1, maxLengthScale, transformDuration));
+            elapsedTime = 0;
+        }
+
+        if (elapsedTime >= powerUpDuration && isPowerActivated)
+        {
+            powerAvailable = false;
+            isPowerActivated = false;
+            StartCoroutine(Transform(maxLengthScale, 1, transformDuration));
+            elapsedTime = 0;
+        }
+
+        if (elapsedTime >= powerUpCoolDown && !isPowerActivated)
+        {
+            powerAvailable = true;
+        }
+    }
+
+    IEnumerator Transform(float from, float to, float duration)
+    {
+        float elapsed = 0;
+        float newScale;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            newScale = from + (to - from) * (elapsed / duration);
+            transform.localScale = new Vector3(1, newScale, 1);
+            yield return null;
+        }
     }
 
     private void FixedUpdate()
