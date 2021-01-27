dd4ae0df3650cac1648219d7449b119048f19a20
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-01-27 13:07:44 +0700

    Optimize trajectory script

--- Assets/Scripts/Trajectory.cs
+++ Assets/Scripts/Trajectory.cs
@@ -5,6 +5,8 @@ public class Trajectory : MonoBehaviour
 
     public BallController ball;
     public GameObject ballAtCollision;
+    public LineRenderer line;
+    public LayerMask layerMask;
 
     private bool drawBallAtCollision = false;
     private Vector2 offsetHitPoint = new Vector2();
@@ -19,56 +21,42 @@ public class Trajectory : MonoBehaviour
 
     void Update()
     {
-        RaycastHit2D[] circleCastHitArray = Physics2D.CircleCastAll(ballRb.position, ballCollider.radius, ballRb.velocity.normalized);
-        Debug.Log(circleCastHitArray.Length);
-        foreach (RaycastHit2D circleCastHit in circleCastHitArray)
+        RaycastHit2D circleCastHit = Physics2D.CircleCast(ballRb.position, ballCollider.radius, ballRb.velocity.normalized, Mathf.Infinity, layerMask);
+
+        if (circleCastHit.collider != null && circleCastHit.collider.GetComponent<BallController>() == null)
         {
+            Vector2 hitPoint = circleCastHit.point;
+            Vector2 hitNormal = circleCastHit.normal;
+            offsetHitPoint = hitPoint + hitNormal * ballCollider.radius;
 
-            Debug.Log(circleCastHit.collider.name);
-            // Jika terjadi tumbukan, dan tumbukan tersebut tidak dengan bola 
-            // (karena garis lintasan digambar dari titik tengah bola)...
-            if (circleCastHit.collider != null && circleCastHit.collider.GetComponent<BallController>() == null)
+            line.positionCount = 2;
+            line.SetPosition(0, ball.transform.position);
+            line.SetPosition(1, offsetHitPoint);
+
+            if (circleCastHit.collider.GetComponent<SideWall>() == null)
             {
-                Vector2 hitPoint = circleCastHit.point;
-                Vector2 hitNormal = circleCastHit.normal;
-                offsetHitPoint = hitPoint + hitNormal * ballCollider.radius;
-                DottedLine.DottedLine.Instance.DrawDottedLine(ball.transform.position, offsetHitPoint);
-                
-                if (circleCastHit.collider.GetComponent<SideWall>() == null)
+                Vector2 inVector = (offsetHitPoint - ball.TrajectoryOrigin).normalized;
+
+                Vector2 outVector = Vector2.Reflect(inVector, hitNormal);
+
+                float outDot = Vector2.Dot(outVector, hitNormal);
+                if (outDot > -1.0f && outDot < 1.0)
                 {
-                    // Hitung vektor datang
-                    Vector2 inVector = (offsetHitPoint - ball.TrajectoryOrigin).normalized;
-
-                    // Hitung vektor keluar
-                    Vector2 outVector = Vector2.Reflect(inVector, hitNormal);
-
-                    // Hitung dot product dari outVector dan hitNormal. Digunakan supaya garis lintasan ketika 
-                    // terjadi tumbukan tidak digambar.
-                    float outDot = Vector2.Dot(outVector, hitNormal);
-                    if (outDot > -1.0f && outDot < 1.0)
-                    {
-                        // Gambar lintasan pantulannya
-                        DottedLine.DottedLine.Instance.DrawDottedLine(
-                            offsetHitPoint,
-                            offsetHitPoint + outVector * 10.0f);
-
-                        // Untuk menggambar bola "bayangan" di prediksi titik tumbukan
-                        drawBallAtCollision = true;
-                    }
+                    line.positionCount = 3;
+                    line.SetPosition(2, offsetHitPoint + outVector * 10.0f);
+                    drawBallAtCollision = true;
                 }
             }
-
         }
+
 
         if (drawBallAtCollision)
         {
-            // Gambar bola "bayangan" di prediksi titik tumbukan
             ballAtCollision.transform.position = offsetHitPoint;
             ballAtCollision.SetActive(true);
         }
         else
         {
-            // Sembunyikan bola "bayangan"
             ballAtCollision.SetActive(false);
         }
     }
